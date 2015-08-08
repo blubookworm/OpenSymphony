171231c464fd0ae62d8090264c7ad4ef9da6fc21
Disha <dishajain2016@gmail.com>
2015-08-08 15:50:57 -0400

    Initial Commit

---
+++ OpenSymphony/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenSymphony
+//
+//  Created by Disha Jain on 8/8/15.
+//  Copyright (c) 2015 Disha Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
